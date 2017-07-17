348906cbb08e904190d0aab1026f66b64f20a26e
Deepak Kumar <Deepaknkumar@users.noreply.github.com>
2017-07-17 00:54:25 -0700

    Module 2: Interface details and nmap scanner
    Module 2 is in Progress

--- Module 2/ifacesdetails.py
+++ Module 2/ifacesdetails.py
@@ -5,7 +5,7 @@ def get_interfaces():
     interfaces = netifaces.interfaces()
     return interfaces
 
-
+# Identifies the gateways
 def get_gateways():
     gateway_dict = {};
     gws = netifaces.gateways()
@@ -18,3 +18,31 @@ def get_gateways():
         except:
             pass
     return gateway_dict
+
+# Identifies the address for each interface
+def get_addresses(interface):
+    addrs = netifaces.ifaddresses(interface)
+    link_addr = addrs[netifaces.AF_LINK]
+    iface_addrs = addrs[netifaces.AF_INET]
+    iface_dict = iface_addrs[0]
+    link_dict = link_addr[0]
+    hwaddr = link_dict.get('addr')
+    iface_addr = iface_dict.get('addr')
+    iface_broadcast = iface_dict.get('broadcast')
+    iface_netmask = iface_dict.get('netmask')
+    return hwaddr, iface_addr, iface_broadcast, iface_netmask
+
+
+def get_networks(gateways_dict):
+    networks_dict = {}
+    for key, value in gateways_dict.items():
+        gateway_ip, iface = value[0], value[1]
+        hwaddress, addr, broadcast, netmask = get_addresses(iface)
+        network = {'gateway': gateway_ip, 'hwaddr': hwaddress, 'addr':addr, 'broadcast':broadcast, 'netmask': netmask}
+        networks_dict[iface] = network
+        return networks_dict
+
+
+gateways = get_gateways()
+network_ifaces = get_networks(gateways)
+print(network_ifaces)
